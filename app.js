ffdff9c444781897d409df3fe1eddb0c74d2ce14
Paul Thomas <paulthomas2107@gmail.com>
2023-11-05 10:00:26 +0000

    xss

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const testBox = document.getElementById('textBox');
+const toFahrenheit = document.getElementById('toFahrenheit');
+const toCelsius = document.getElementById('toCelsius');
+const result = document.getElementById('result');
+
+let temp;
+
 const convert = () => {
-  console.log('Convert');
+  if (toFahrenheit.checked) {
+    temp = Number(testBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + '°F';
+  } else if (toCelsius.checked) {
+    temp = Number(testBox.value);
+    temp = ((temp - 32) * 5) / 9;
+    result.textContent = temp.toFixed(1) + '°C';
+  } else {
+    result.textContent = 'Select a unit.';
+  }
 };
